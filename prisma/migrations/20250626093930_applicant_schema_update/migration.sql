dce0cd0cac5fe2835acbc9b6307faa3db8c4bc71
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-06-26 15:11:23 +0530

    applicant-table-update

---
+++ prisma/migrations/20250626093930_applicant_schema_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `resumeUrl` on the `ApplicantProfile` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ApplicantProfile" DROP COLUMN "resumeUrl";
