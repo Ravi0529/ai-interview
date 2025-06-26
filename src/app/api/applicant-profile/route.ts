224dc9d328260ec36445ba9b1dc2392614b65bcc
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-06-26 19:28:50 +0530

    RBAC-profile-routes

---
+++ src/app/api/applicant-profile/route.ts
@@ -0,0 +1,150 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export const GET = async (req: NextResponse) => {
+  const userId = (await auth()).userId;
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const profile = await prisma.recruiterProfile.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (!profile) {
+      return NextResponse.json(
+        {
+          error: "Profile not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(profile);
+  } catch (error) {
+    console.error("Error fetching applicant profile:", error);
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+export const POST = async (req: NextResponse) => {
+  const userId = (await auth()).userId;
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const {
+      phone,
+      age,
+      education,
+      instituteName,
+      currentCompany,
+      currentStatus,
+      experience,
+      jobPreferences,
+      linkedInProfile,
+      xProfile,
+      githubProfile,
+      city,
+      state,
+    } = await req.json();
+
+    if (
+      !phone ||
+      !age ||
+      !education ||
+      !instituteName ||
+      !currentStatus ||
+      !experience ||
+      !jobPreferences ||
+      !city ||
+      !state
+    ) {
+      return NextResponse.json(
+        {
+          error:
+            "Missing required fields: Phone number, Age, Institute Name, Current Status, Experience, Job Preferences, city, and state",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const existingProfile = await prisma.recruiterProfile.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    const payload = {
+      phone,
+      age,
+      education,
+      instituteName,
+      currentStatus,
+      experience,
+      jobPreferences,
+      ...(currentCompany && { currentCompany }),
+      ...(linkedInProfile && { linkedInProfile }),
+      ...(xProfile && { xProfile }),
+      ...(githubProfile && { githubProfile }),
+      city,
+      state,
+      userId,
+    };
+
+    const profile = existingProfile
+      ? await prisma.applicantProfile.update({
+          where: {
+            userId,
+          },
+          data: payload,
+        })
+      : await prisma.applicantProfile.create({
+          data: payload,
+        });
+
+    return NextResponse.json(profile);
+  } catch (error) {
+    console.error("Error creating/updating applicant profile:", error);
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
---
+++ src/app/api/recruiter-profile/route.ts
@@ -0,0 +1,118 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export const GET = async (req: NextResponse) => {
+  const userId = (await auth()).userId;
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const profile = await prisma.applicantProfile.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (!profile) {
+      return NextResponse.json(
+        {
+          error: "Profile not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return NextResponse.json(profile);
+  } catch (error) {
+    console.error("Error fetching applicant profile:", error);
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+export const POST = async (req: NextResponse) => {
+  const userId = (await auth()).userId;
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const { companyName, companyWebsite, industry, position, linkedInProfile } =
+      await req.json();
+
+    if (!companyName || !industry) {
+      return NextResponse.json(
+        {
+          error: "Missing required fields: companyName or industry",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const existingProfile = await prisma.applicantProfile.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    const payload = {
+      companyName,
+      industry,
+      ...(companyWebsite && { companyWebsite }),
+      ...(position && { position }),
+      ...(linkedInProfile && { linkedInProfile }),
+      userId,
+    };
+
+    const profile = existingProfile
+      ? await prisma.applicantProfile.update({
+          where: {
+            userId,
+          },
+          data: payload,
+        })
+      : await prisma.applicantProfile.create({
+          data: payload,
+        });
+
+    return NextResponse.json(profile);
+  } catch (error) {
+    console.error("Error creating/updating applicant profile:", error);
+    return NextResponse.json(
+      {
+        error: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
