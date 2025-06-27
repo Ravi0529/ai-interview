a3793906aea6cfcfb4e00d382ff152fbad8bd9bd
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-06-27 23:58:43 +0530

    job-application-table-added

---
+++ prisma/migrations/20250627182651_job_application_added/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "ApplicationStatus" AS ENUM ('Applied', 'Reviewing', 'Accepted', 'Rejected');
+
+-- CreateEnum
+CREATE TYPE "WorkStatus" AS ENUM ('Remote', 'Offline');
+
+-- CreateTable
+CREATE TABLE "Job" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "industry" "Industry" NOT NULL,
+    "experience" "Experience" NOT NULL,
+    "salary" TEXT,
+    "requiredSkills" TEXT[],
+    "workStatus" "WorkStatus" NOT NULL,
+    "createdById" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" TEXT NOT NULL,
+    "status" "ApplicationStatus" NOT NULL DEFAULT 'Applied',
+    "jobId" TEXT NOT NULL,
+    "applicantId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Job" ADD CONSTRAINT "Job_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Application" ADD CONSTRAINT "Application_applicantId_fkey" FOREIGN KEY ("applicantId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
