550f12cb658cd4ce766e7d2a9341b431ad3c8cfa
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-06-28 15:34:59 +0530

    GET-all-jobs-for-recruiter

---
+++ src/app/api/recruiter-posts/jobs/route.ts
@@ -0,0 +1,66 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { Role } from "@prisma/client";
+
+// GET all jobs posted by particular recruiter (Only Recruiter)
+export const GET = async () => {
+  const userId = (await auth()).userId;
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        role: true,
+      },
+    });
+
+    if (!user || user.role !== Role.recruiter) {
+      return NextResponse.json(
+        {
+          error: "Forbidden",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const jobs = await prisma.job.findMany({
+      where: {
+        createdById: userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        _count: {
+          select: {
+            applications: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(jobs);
+  } catch (error) {
+    console.error("Error fetching recruiter jobs:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+};
